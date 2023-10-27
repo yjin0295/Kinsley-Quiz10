afc4cd5eee4d8b94bf9de881b960a5de03c9d36e
yjin0295 <yjin0295@uni.sydney.edu.au>
2023-10-27 15:10:25 +1100

    revise

--- sketch.js
+++ sketch.js
@@ -1,104 +1,58 @@
-var song;
-var fft;
-var particles = [];
-var img;
-var amp;
-var spectralCentroid; // Variable to store spectral centroid
-var previousSpectrum; // Array to store the previous spectrum frame
+// Declare variables for audio and FFT analysis
+let sound;  // Audio file
+let fft;    // Fast Fourier Transform analysis
+let band = 1024;  // Number of frequency bands
+let rot = [];     // Array to store rotation values for each band
 
 function preload() {
-  // Preload audio and image
-  alert("Click on the screen to play or pause")
-  song = loadSound("asset/sample-visualisation.mp3")
-  img = loadImage("asset/photo-1482686115713-0fbcaced6e28.avif")
+  // Load the audio file before setup
+  sound = loadSound('asset/sample-visualisation.mp3');
 }
 
 function setup() {
-  // Setup the canvas and initial settings
+  // Set up the canvas and initial parameters
   createCanvas(windowWidth, windowHeight);
-  angleMode(DEGREES);
-  imageMode(CENTER);
-  colorMode(HSB);
-  rectMode(CENTER);
+  colorMode(HSB, 360, 256, 256, 256);  // Color mode
+  fft = new p5.FFT(0.0, band);       // FFT analysis object
+  fft.setInput(sound);               // Analyze the loaded sound
 
-  // Create the FFT (Fast Fourier Transform) object
-  fft = new p5.FFT(0.9, 512);
-  img.filter(BLUR, 3);
-  noLoop();
-
-  // Initialize the previous spectrum with zeros
-  previousSpectrum = new Array(512).fill(0);
+  // Initialize rotation values for each band
+  for (let i = 0; i < band; i++) {
+    rot[i] = random(360);
+  }
+  background(0);  // Set the initial background color to black
 }
 
 function draw() {
-  // Clear the background
-  background(0);
-  var spectrum = fft.analyze();
-  translate(width / 2, height / 2);
+  // Clear a portion of the canvas and set blend mode for drawing
+  blendMode(BLEND);
+  background(0, 0, 0, 10);  // Slight background fade effect
+  blendMode(ADD);  // Additive blending for visual effect
+  noStroke();  // No outlines for shapes
+  let spectrum = fft.analyze();  // Analyze audio spectrum
+  translate(width/2, height/2);  // Center drawing on canvas
 
-  // Get the audio amplitude in a specific frequency range
-  amp = fft.getEnergy(20, 200);
+  // Loop through each frequency band
+  for (let i = 0; i < spectrum.length; i++) {
+    let x = map(log(i), 0, log(spectrum.length), 0, height);
+    let h = map(log(i), 0, log(spectrum.length), 0, 255);
+    let diameter = map(pow(spectrum[i], 2), 0, pow(255, 2), 1, 40);
+    let r = map(spectrum[i], 0, 255, 0.01, 1.0);
 
-  // Calculate the spectral centroid
-  spectralCentroid = fft.getCentroid();
-
-  // Rotate the image if the amplitude is high
-  push();
-  if (amp > 230) {
-    rotate(random(-1, 1));
+    rot[i] += r;  // Update rotation value
+    push();
+    rotate(radians(rot[i]));  // Apply rotation
+    fill(h, 190, 127);  // Set circle color
+    circle(x, 0, diameter);  // Draw the circle
+    pop();  // Restore previous transformation state
   }
-  image(img, 0, 0, width + 100, height + 100);
-  pop();
-
-  // Draw a background shape
-  colorMode(RGB);
-  fill(33);
-  noStroke();
-  rect(0, 0, width, height);
-  colorMode(HSB);
-
-  strokeWeight(3);
-  noFill();
-
-  // Calculate Spectral Flux
-  var spectralFlux = 0;
-  for (var i = 0; i < spectrum.length; i++) {
-    var flux = spectrum[i] - previousSpectrum[i];
-    spectralFlux += flux > 0 ? flux : 0;
-  }
-  previousSpectrum = spectrum.slice(); // Update the previous spectrum
-
-  // Draw spectral lines
-  for (var i = 0; i < spectrum.length; i += 6) {
-    var angle = map(i, 1, spectrum.length, 0, 360) - 90;
-    var amp2 = spectrum[i];
-    var r = map(amp2, 0, 256, 80, 250);
-    var x = r * sin(angle);
-    var y = r * cos(angle);
-
-    line(0, 0, x, y);
-    line(x * 1.05, y * 1.05, x * 1.06, y * 1.06);
-    stroke(i / 1.4, 200, 200);
-  }
-  noStroke();
-  colorMode(RGB);
-  fill(33);
-  circle(0, 0, 155);
-
-  // Display the spectral centroid and spectral flux values
-  fill(255);
-  textSize(18);
-  text('Spectral Centroid: ' + round(spectralCentroid) + ' Hz', 150, 20);
-  text('Spectral Flux: ' + round(spectralFlux), 150, 40);
 }
 
 function mouseClicked() {
-  // Handle mouse click to play or pause the song
-  if (song.isPlaying()) {
-    song.pause();
-    noLoop();
+  if (sound.isPlaying() == false) {
+    sound.loop();  // Start playing the audio in a loop
   } else {
-    song.play();
-    loop();
+    sound.stop();  // Stop playing the audio
   }
 }
+
